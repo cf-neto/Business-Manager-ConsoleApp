f85351727d235c7bb3b4836978beebb05f363e77
cf-neto <carlosfdsn2008@gmail.com>
2025-05-03 17:26:51 -0300

    Feat: melhorando visualização de funções

--- Program.cs
+++ Program.cs
@@ -6,6 +6,41 @@ namespace Estudando
 {
     class Program
     {
+        static void MostrarMenu()
+        {
+            Console.WriteLine("=== Business Manager ===");
+            Console.WriteLine("1. Adicionar Funcionário");
+            Console.WriteLine("2. Listar Funcionários");
+            Console.WriteLine("3. Remover Funcionários");
+            Console.WriteLine("4. Procurar Funcionário");
+            Console.WriteLine("5. Pagar Salário");
+            Console.WriteLine("0. Sair");
+            Console.Write("\nEscolha uma opção: ");
+        }
+
+        static void ObterOpcaoMenu()
+        {
+            int opcao = -1;
+            bool entradaValida = false;
+
+            while (!entradaValida)
+            {
+                try
+                {
+                    int opcao = int.Parse(Console.ReadLine());
+                    entradaValida = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Por favor, insira um número válido.");
+                    MostrarMenu();
+                }
+            }
+
+            return opcao;
+        }
+
+
         static void Main(string[] args)
         {
             bool executando = true;
@@ -14,16 +49,11 @@ namespace Estudando
 
             while (executando)
             {
-                Console.WriteLine("=== Business Manager ===");
-                Console.WriteLine("1. Adicionar Funcionário");
-                Console.WriteLine("2. Listar Funcionários");
-                Console.WriteLine("3. Remover Funcionários");
-                Console.WriteLine("4. Procurar Funcionário");
-                Console.WriteLine("5. Pagar Salário");
-                // Console.WriteLine("6. Relatório"); 
-                Console.WriteLine("0. Sair");
-                Console.Write("\nEscolha uma opção: ");
-                int opcao = int.Parse(Console.ReadLine());
+                // Mostrar opções
+                MostrarMenu();
+
+                // Validar se a entrada é um número válido
+                int opcao = ObterOpcaoMenu();
 
                 switch (opcao)
                 {
